b81ac22d418791bd49aa92c256e0880456f03d4d
Iver Oknes <iver@oknes.no>
2025-04-06 00:07:38 +0200

    Make field line calculations length based

--- field_line.py
+++ field_line.py
@@ -12,7 +12,7 @@ def f(p, ux, uy, interpolate=True):
         return np.array([0,0])
     return np.array([ux[*pr], uy[*pr]])
 
-def field_line(ux, uy, p0, nstep, dx=0.1, integrator=forward_euler, backwards=False):
+def field_line(ux, uy, p0, l, dx=0.2, integrator=forward_euler, backwards=False):
     """
     ## Parameters:
         - l_max: length of field line in either direction
@@ -20,34 +20,47 @@ def field_line(ux, uy, p0, nstep, dx=0.1, integrator=forward_euler, backwards=Fa
     line_f = [p0]
     line_b = [p0]
     length = 0
-    #while length < l:
-    for i in range(nstep):
-        p_next = integrator(line_f[-1], f, ux, uy, dx, backwards=False)
-        length += np.linalg.norm(p_next - line_f[-1])
-        line_f.append(p_next)
-        if backwards:
+    f_stall = False
+    b_stall = not backwards
+    while length < l:
+        if not f_stall:
+            p_next = integrator(line_f[-1], f, ux, uy, dx, backwards=False)
+            if np.all(p_next - line_f[-1] == 0):
+                f_stall = True
+            length += np.linalg.norm(p_next - line_f[-1])
+            print(f"length ({length}) += {p_next - line_f[-1]} fstall: {f_stall}")
+            line_f.append(p_next)
+        if backwards and not b_stall:
             pb_next = integrator(line_b[-1], f, ux, uy, dx, backwards=True)
+            if np.all(pb_next - line_b[-1] == 0):
+                b_stall = True
             length += np.linalg.norm(pb_next - line_b[-1])
+            print(f"length ({length}) += {pb_next - line_b[-1]} bstall: {b_stall}")
             line_b.append(pb_next)
+        if f_stall and b_stall:
+            break
 
     return np.array(line_b[-1:0:-1] + line_f)
 
 def main():
-    ux, uy = read_h5_yx("data/isabel_2d.h5")
-    #ux, uy = read_h5("data/metsim1_2d.h5")
+    #ux, uy = read_h5_yx("data/isabel_2d.h5")
+    ux, uy = read_h5("data/metsim1_2d.h5")
     x = np.arange(ux.shape[0])
     y = np.arange(ux.shape[1])
     x, y = np.meshgrid(x, y)
 
-    fig, ax = plt.subplots(1, 1)
-    #n = 10
-    #plt.quiver(x[::n,::n], y[::n,::n], ux[::n,::n], uy[::n,::n], scale=1000)
-    p0 = seed_uniform(0, 499, 0, 499, 20)
+    fig, ax = plt.subplots(1, 3)
+    n = 5
+    ax[2].quiver(x[::n,::n], y[::n,::n], ux[::n,::n], uy[::n,::n], scale=1000)
+    #p0 = seed_uniform(0, 499, 0, 499, 20)
+    p0 = seed_uniform(0, 126, 0, 126, 20)
     #p0 = seed_random(0, 499, 0, 499, 400)
-    fl = [field_line(ux, uy, i, 1000, integrator=forward_euler, backwards=True) for i in p0]
+    fl = [field_line(ux, uy, i, 20, integrator=forward_euler, backwards=False) for i in p0]
+    flb = [field_line(ux, uy, i, 20, integrator=forward_euler, backwards=True) for i in p0]
     #fl = [field_line(ux, uy, i, 1000, integrator=runge_kutta_4, backwards=True) for i in p0]
-    for i in fl:
-        ax.plot(i[:,0], i[:,1])
+    for i, j in zip(fl, flb):
+        ax[0].plot(i[:,0], i[:,1])
+        ax[1].plot(j[:,0], j[:,1])
     plt.show()
 
 if __name__ == "__main__":

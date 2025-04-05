8b032743cc5955a74e8293c534a344313318c2af
Iver Oknes <iver@oknes.no>
2025-04-05 19:35:47 +0200

    Update spread of uniform seeding and number of random seed

--- lic.py
+++ lic.py
@@ -3,12 +3,5 @@ import numpy as np
 def noise_texture(nx, ny):
     return (255 * np.random.rand(nx, ny)).astype(int)
 
-def field_line(L):
-    """
-    ## Parameters:
-        - L: length of field line in either direction
-    """
-    pass
-
 def lic(filter: np.matrix):
     pass
--- seeding.py
+++ seeding.py
@@ -5,19 +5,16 @@ def seed_uniform(xmin, xmax, ymin, ymax, n):
     Create seeding points uniformly in the specified range.
     A total n^2 points are created.
     """
-    stepx = (xmax - xmin)/n
-    stepy = (ymax - ymin)/n
-    x = np.arange(xmin, xmax, stepx)
-    y = np.arange(ymin, ymax, stepy)
+    x = np.linspace(ymin, ymax, n)
+    y = np.linspace(ymin, ymax, n)
     x, y = np.meshgrid(x, y)
     return np.stack([x.flat, y.flat], axis=1)
 
 def seed_random(xmin, xmax, ymin, ymax, n):
     """
     Create seeding points randomly in the specified range.
-    A total of n^2 points are created.
+    A total of n points are created.
     """
     x = np.random.uniform(xmin, xmax, n)
     y = np.random.uniform(ymin, ymax, n)
-    x, y = np.meshgrid(x, y)
-    return np.stack([x.flat, y.flat], axis=1)
+    return np.stack([x,y], axis=1)

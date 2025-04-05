39473154c8c5b370ad206910327b0edb57fda258
Iver Oknes <iver@oknes.no>
2025-04-05 19:35:17 +0200

    Add backwards integration of field lines

--- field_line.py
+++ field_line.py
@@ -2,27 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from read_data import read_h5, read_h5_yx
-#from integration import forward_euler, runge_kutta_4
+from seeding import seed_uniform, seed_random
+from integration import forward_euler, runge_kutta_4
 
-def f(p, ux, uy):
-    p = np.array([round(p[1]), round(p[0])])
+def f(p, ux, uy, interpolate=True):
+    # If out of bounds, return 0s
     if p[0] < 0 or p[0] >= ux.shape[0] or p[1] < 0 or p[1] >= ux.shape[1]:
         return np.array([0,0])
+    p = np.array([round(p[1]), round(p[0])])
     return np.array([ux[*p], uy[*p]])
 
-def field_line(ux, uy, p0, nstep, dx=1, integrator=None):
+def field_line(ux, uy, p0, nstep, dx=0.1, integrator=forward_euler, backwards=False):
     """
     ## Parameters:
         - l_max: length of field line in either direction
     """
-    line = [p0]
+    line_f = [p0]
+    line_b = [p0]
     length = 0
     #while length < l:
     for i in range(nstep):
-        dl = dx * f(line[-1], ux, uy)
-        length += np.linalg.norm(dl)
-        line.append(line[-1] + dl)
-    return np.array(line)
+        p_next = integrator(line_f[-1], f, ux, uy, dx, backwards=False)
+        length += np.linalg.norm(p_next - line_f[-1])
+        line_f.append(p_next)
+        if backwards:
+            pb_next = integrator(line_b[-1], f, ux, uy, dx, backwards=True)
+            length += np.linalg.norm(pb_next - line_b[-1])
+            line_b.append(pb_next)
+        if np.all(line_f[-1] == line_f[-2]):
+            # End calculation if the line has not moved
+            return np.array(line_b[-1:0:-1] + line_f)
+        if len(line_b) > 1:
+            if np.all(line_b[-1] == line_b[-2]):
+                return np.array(line_b[-1:0:-1] + line_f)
+
+    return np.array(line_b[-1:0:-1] + line_f)
 
 def main():
     ux, uy = read_h5_yx("data/isabel_2d.h5")
@@ -30,20 +44,16 @@ def main():
     x = np.arange(ux.shape[0])
     y = np.arange(ux.shape[1])
     x, y = np.meshgrid(x, y)
-    f1 = field_line(ux, uy, [340, 280], 100)
-    print(f"f1 len: {len(f1)}")
 
     fig, ax = plt.subplots(1, 1)
-    n = 10
-    plt.quiver(x[::n,::n], y[::n,::n], ux[::n,::n], uy[::n,::n], scale=1000)
-    #ax.plot(f1[:,0], f1[:,1])
-    #p0 = [[499, i*50] for i in range(10)]
-    p0 = [[i*50, 100] for i in range(10)]
-    fl = [field_line(ux, uy, i, 100) for i in p0]
+    #n = 10
+    #plt.quiver(x[::n,::n], y[::n,::n], ux[::n,::n], uy[::n,::n], scale=1000)
+    p0 = seed_uniform(0, 499, 0, 499, 20)
+    #p0 = seed_random(0, 499, 0, 499, 400)
+    fl = [field_line(ux, uy, i, 1000, integrator=forward_euler, backwards=True) for i in p0]
+    #fl = [field_line(ux, uy, i, 1000, integrator=runge_kutta_4, backwards=True) for i in p0]
     for i in fl:
         ax.plot(i[:,0], i[:,1])
-    #ax.invert_yaxis()
-    #ax.streamplot(x, y, ux, uy)
     plt.show()
 
 if __name__ == "__main__":
--- integration.py
+++ integration.py
@@ -1,7 +1,22 @@
 import numpy as np
+from numba import jit
 
-def forward_euler():
-    return np.array()
 
-def runge_kutta_4():
-    pass
+def forward_euler(p, f, ux, uy, dx, backwards=False):
+    if backwards:
+        return p - dx * f(p, ux, uy)
+    return p + dx * f(p, ux, uy)
+
+
+def runge_kutta_4(p, f, ux, uy, dx, backwards=False):
+    if backwards:
+        K1 = dx*f(p, ux, uy)
+        K2 = dx*f(p - 0.5*K1, ux, uy)
+        K3 = dx*f(p - 0.5*K2, ux, uy)
+        K4 = dx*f(p - K3, ux, uy)
+        return p - (1/6.0)*(K1 + 2*K2 + 2*K3 + K4)
+    K1 = dx*f(p, ux, uy)
+    K2 = dx*f(p + 0.5*K1, ux, uy)
+    K3 = dx*f(p + 0.5*K2, ux, uy)
+    K4 = dx*f(p + K3, ux, uy)
+    return p + (1/6.0)*(K1 + 2*K2 + 2*K3 + K4)

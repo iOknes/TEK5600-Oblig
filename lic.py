36b7b3b0c8caf8a0334afb5f73715a4b59093acc
Iver Oknes <iver@oknes.no>
2025-04-06 22:46:58 +0200

    Update with bugfixes

--- lic.py
+++ lic.py
@@ -1,7 +1,29 @@
 import numpy as np
 
-def noise_texture(nx, ny):
-    return (255 * np.random.rand(nx, ny)).astype(int)
+from field_line import field_line
+from integration import forward_euler
 
-def lic(filter: np.matrix):
+def noise_texture(nx, ny, level=255):
+    return (level * np.random.rand(nx, ny)).astype(int)
+
+def convolute():
     pass
+
+def lic(ux, uy, image: np.ndarray, line_len=0.5, integrator=forward_euler):
+    output = np.zeros(image.shape)
+    fll = line_len * np.average(ux.shape)
+    lx, ly = ux.shape
+    for i, row in enumerate(image):
+        for j, col in enumerate(row):
+            p = [round(i * image.shape[0] / ux.shape[0]), round(j * image.shape[1] / ux.shape[1])]
+            fl = field_line(ux, uy, p, fll, dx=0.5, integrator=integrator, backwards=True)
+            fl_coords = np.clip(np.round(fl).astype(int), 0, image.shape[0]-1)
+            output[i,j] = np.mean(image[fl_coords])
+    output = (output - output.min())
+    return output / output.max()
+
+def plot_lic(ax, lic_image, metsim=False):
+    if metsim:
+        ax.imshow(lic_image.T, cmap="grey")
+    else:
+        ax.imshow(lic_image, cmap="grey")
